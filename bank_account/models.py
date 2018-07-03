53b182d87cfe0798fd48c47977501e77d46e2c1c
Chiragitr <chirag.guptaitr@gmail.com>
2018-07-03 18:44:55 +0530

    foreign key added

--- bank_account/models.py
+++ bank_account/models.py
@@ -2,8 +2,10 @@
 from __future__ import unicode_literals
 from django.db import models 
 from django_extensions.db.models import TimeStampedModel
+from accounts.models import User
 
 class bank_account(TimeStampedModel):
+	user = models.ForeignKey(User,default="")
 	account_holder_name = models.CharField(max_length = 200)
 	account_number = models.IntegerField()
 	ifsc_code = models.IntegerField()
