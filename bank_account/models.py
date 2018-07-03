cf8aa473e4e362319f5a8ac7696c261c02420cff
rahul kumar <rahul.sanoriaitr@gmail.com>
2018-07-03 03:21:38 +0000

    models.py edited online with Bitbucket

--- bank_account/models.py
+++ bank_account/models.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models 
+from django_extensions.db.models import TimeStampedModel
 
 
-class bank_account(models.Model):
+
+class bank_account(TimeStampedModel):
 	account_holder_name = models.CharField(max_length = 200)
 	account_number = models.IntegerField()
 	ifsc_code = models.IntegerField()
